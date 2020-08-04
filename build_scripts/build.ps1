83363d35bd14ce199b989b12863c6955a486b08c
AjayKMehta <AjayMehta1@gmail.com>
2020-08-03 20:47:09 -0700

    Move tool install logic back to yaml.

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -1,10 +1,3 @@
-if (dotnet tool list --tool-path ./tools | Select-String 'minver-cli') {
-    Write-Host 'minver-cli already installed.' -ForegroundColor Blue
-} else {
-    Write-Host 'Installing minver-cli' -ForegroundColor Yellow
-    dotnet tool install --tool-path ./tools --verbosity detailed minver-cli
-}
-
 $env:BUILDVER = minver
 Write-Host 'Calculated version number is ' $env:BUILDVER -ForegroundColor DarkGreen
 $buildVersion = $env:BUILDVER
